11eb549f88686360de8975d62cff7d3f87594b3a
MYangGit <93202929+MYangGit@users.noreply.github.com>
2023-05-03 13:39:01 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+//app.js
+App({
+  onLaunch: function () {
+   
+  },
+  _getUserInfo: function () {
+    var userInfoStorage = wx.getStorageSync('user');
+    if (!userInfoStorage) {
+      var that = this;
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res);
+              that.globalData.g_userInfo = res.userInfo
+              wx.setStorageSync('user', res.userInfo)
+            },
+            fail: function (res) {
+              console.log(res);
+            }
+          })
+        }
+      })
+    }
+    else {
+      this.globalData.g_userInfo = userInfoStorage;
+    }
+  },
+  globalData: {
+    g_isPlayingMusic: false,
+    g_currentMusicPostId: null,
+   // doubanBase: "https://api.douban.com",
+    doubanBase: "https://douban.uieee.com/",
+    g_userInfo: null
+  }
+})
